3633aaa9e51a67f528915b55c4565dd31517a7fe
Krishna Tulsyan <kt.krishnatulsyan@gmail.com>
2015-07-06 18:32:50 +0530

    cleaned login.py -- Finished

--- TheApp/login.py
+++ TheApp/login.py
@@ -15,11 +15,12 @@ form3_class = uic.loadUiType("dialogBox.ui")[0]
 class Dialog(QtGui.QDialog,form3_class):
     """A simple Media Player using VLC and Qt
     """
-    def __init__(self, master=None):
+    def __init__(self, message='', master=None):
         QtGui.QMainWindow.__init__(self, master)
         self.setupUi(self)
         self.setWindowTitle("DialogBox")
-        self.message = ''
+        self.message = message
+        self.setLabel()
 
     def setMessage(self,msg):
         self.message=msg
@@ -27,7 +28,6 @@ class Dialog(QtGui.QDialog,form3_class):
 
     def setLabel(self):
         self.displayText.setText(str(self.message))
-
 
 
 class Register(QtGui.QDialog,form2_class):
@@ -41,11 +41,8 @@ class Register(QtGui.QDialog,form2_class):
         self.cancelButton.clicked.connect(self.close)
 
     def register(self):
-        usernameValue =str(self.username.toPlainText()).strip()
-        passwordValue =str(self.password.toPlainText()).strip()
-        emailValue =str(self.email.toPlainText()).strip()
-        fnameValue =str(self.fname.toPlainText()).strip()
-        lnameValue =str(self.lname.toPlainText()).strip()
+        for i in ('username','password','email','fname','lname'):
+            exec ("%sValue = str(self.%s.toPlainText()).strip()" % (i,i))
         isTA = self.checkBox.isChecked()
         if all(a != '' for a in (usernameValue, passwordValue, emailValue, fnameValue, lnameValue)):
             payload = {
@@ -59,24 +56,18 @@ class Register(QtGui.QDialog,form2_class):
             url = 'http://localhost:8000/polls/Register/'
             r=requests.get(url,params=payload)
             if int(r.status_code) == 500:
-                self.dlg = Dialog()
-                self.dlg.setMessage('Username already aquired...')
+                self.dlg = Dialog('Username already aquired...')
                 self.dlg.show()
             else:
-                self.dlg = Dialog()
-                self.dlg.setMessage(r.text+' successfully registered')
+                self.dlg = Dialog(r.text+' successfully registered', self)
                 self.dlg.show()
                 self.hide()
-        else:
-            self.dlg = Dialog()
-            self.dlg.setMessage('Field Missing!')
-            self.dlg.show()
+        else: Dialog('Field Missing!', self).show()
 
     def closeEvent(self, event):
         reply = QtGui.QMessageBox.question(self, 'Message',
             "Are you sure to quit?", QtGui.QMessageBox.Yes | 
             QtGui.QMessageBox.No, QtGui.QMessageBox.No)
-
         if reply == QtGui.QMessageBox.Yes:
             event.accept()
         else:
@@ -100,33 +91,24 @@ class Login(QtGui.QMainWindow,form_class):
         usernameValue =str(self.username.toPlainText()).strip()
         passwordValue =str(self.password.toPlainText()).strip()
         if usernameValue == '' or passwordValue == '':
-#           i=0   #trying to make multiple dialogs
-#           self.dlgw.append(Dialog())
-#           self.dlgw[i].setMessage('Feild Missing')
-#           self.dlgw[i].show()
-#           i+=1
-            self.dlg=Dialog()
-            self.dlg.setMessage('Feild Missing')
+            self.dlg=Dialog('Field Missing!')
             self.dlg.show()
-        
         else:
-            payload={'username':str(usernameValue), 'pass':str(passwordValue)}
+            payload = {
+                    'username' : str (usernameValue),
+                    'pass'     : str (passwordValue)
+                    }
             url = 'http://localhost:8000/polls/Login/'
-            r=requests.get(url,params=payload)
+            r = requests.get(url,params=payload)
             print r.text
             if r.text == '':
-                self.dlg = Dialog()
-                self.dlg.setMessage("username or password incorrect")
+                self.dlg = Dialog("username or password incorrect")
                 self.dlg.show()
             else:
                 # Login Successfull
                 self.sessionid = r.cookies['sessionid']
-#               self.dlg = Dialog()
-#               data = json.loads(r.text)
-#               self.dlg.setMessage(data['uname'] + " successfully logged in")
-#               self.dlg.show()
-               # self is passed in the chat to gain the access to
-               # self.sessionid variable..
+                # self is passed in the chat to gain the access to
+                # self.sessionid variable..
                 self.chat = Chat(self)
                 self.chat.show()
                 self.hide()
@@ -142,12 +124,10 @@ class Login(QtGui.QMainWindow,form_class):
         reply = QtGui.QMessageBox.question(self, 'Message',
             "Are you sure to quit?", QtGui.QMessageBox.Yes | 
             QtGui.QMessageBox.No, QtGui.QMessageBox.No)
-
         if reply == QtGui.QMessageBox.Yes:
             event.accept()
         else:
             event.ignore()       
-
 
 
 if __name__ == "__main__":
