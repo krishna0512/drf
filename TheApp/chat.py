f421bda89634dac34af524947b09c467c7227fb6
Krishna Tulsyan <kt.krishnatulsyan@gmail.com>
2015-07-07 19:12:11 +0530

    Improved the scrolling in chat and added the error message in case of tag error

--- TheApp/chat.py
+++ TheApp/chat.py
@@ -6,6 +6,25 @@ from StringIO import StringIO
 from PyQt4 import QtGui, QtCore, uic
 
 form_class = uic.loadUiType("chat.ui")[0]
+form3_class = uic.loadUiType("dialogBox.ui")[0]
+
+class Dialog(QtGui.QDialog,form3_class):
+    """A simple Media Player using VLC and Qt
+    """
+    def __init__(self, message='', master=None):
+        QtGui.QMainWindow.__init__(self, master)
+        self.setupUi(self)
+        self.setWindowTitle("DialogBox")
+        self.message = message
+        self.setLabel()
+
+    def setMessage(self,msg):
+        self.message=msg
+        self.setLabel()
+
+    def setLabel(self):
+        self.displayText.setText(str(self.message))
+
 
 class Chat(QtGui.QMainWindow,form_class):
     """
@@ -23,6 +42,8 @@ class Chat(QtGui.QMainWindow,form_class):
         self.timer.setInterval(500)
         self.timer.timeout.connect (self.updateMessage)
 
+        #self.isChat.toggle()
+
         # This variable is derived from Login class of temp
         self.sessionid = str(master.sessionid)
 
@@ -39,7 +60,7 @@ class Chat(QtGui.QMainWindow,form_class):
 
     def submit(self):
         payload={'name':self.username, 'message':str(self.textArea.toPlainText()).strip()}
-        if str(self.textArea.toPlainText()).strip()!='':
+        if str(self.textArea.toPlainText()).strip() is not '':
             if self.isQues.isChecked():
                 payload['isQues'] = True
                 payload['isAns' ] = False
@@ -52,20 +73,26 @@ class Chat(QtGui.QMainWindow,form_class):
                 payload['isAns' ] = False
             url = 'http://localhost:8000/polls/PostInsertQuery/'
             data = {'data':json.dumps(payload)}
-            requests.get(url,params = data)
-            self.textArea.setText('')
+            r = requests.get(url,params = data)
+            # Checks if the request is processed correctly by the server
+            if int(r.status_code) == 500:
+                d = Dialog ('Invalid Tag Number! Please Try Again..',self)
+                d.show()
+                self.tagArea.clear()
+            else:
+                self.textArea.setText('')
             # updates the messages retrived from the server
             self.updateMessage()
    
     def updateMessage(self):
         # Get all the messages from the database
         url = 'http://localhost:8000/polls/GetInsertQuery/'
-        message=str(requests.get(url).text)
-
-    	self.textBrowser.setText(message)
-
-        # Moves the scroll to the bottom
-    	self.textBrowser.moveCursor(QtGui.QTextCursor.End)
+        message = str(requests.get(url).text).strip()
+        # This is checking and updating the messages only when there is a change
+        if str(self.textBrowser.toPlainText()).strip() != message:
+    	    self.textBrowser.setText(message)
+            # Moves the scroll to the bottom
+            self.textBrowser.moveCursor(QtGui.QTextCursor.End)
 
 
 if __name__ == "__main__":
